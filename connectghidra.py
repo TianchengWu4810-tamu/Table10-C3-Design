1b422989413f3fb8a347ce809e6a2ca605c449f0
“dvh5561” <dvh5561@tamu.edu>
2023-11-27 22:41:02 -0600

    more lines

--- connectghidra.py
+++ connectghidra.py
@@ -1,5 +1,12 @@
-# import ghidra
-import ghidra.app.script.GhidraScript
+import ghidra
+from ghidra.app.decompiler import DecompileOptions
+from ghidra.app.decompiler import DecompInterface
+from ghidra.util.task import ConsoleTaskMonitor
+
+# import ghidra.app.script.GhidraScript
+# from ghidra.app.util.datatype import DataTypeSelectionDialog
+# from ghidra.util.data.DataTypeParser import AllowedDataTypes
+
 
 state = getState()
 currentProgram = state.getCurrentProgram()
@@ -8,7 +15,21 @@ location = currentProgram.getExecutablePath()
 # print("The currently loaded program is: '{}'".format(name))
 # print("Its location on disk is: '{}'".format(location))
 
+options = DecompileOptions()
+monitor = ConsoleTaskMonitor()
+ifc = DecompInterface()
+ifc.setOptions(options)
+ifc.openProgram(currentProgram)
+
+funcDicts = []
 fm = currentProgram.getFunctionManager()
-funcs = fm.getFunctions(True)  # True means 'forward'
+funcs = fm.getFunctions(True)
 for func in funcs:
-    print("Function: {} @ 0x{}".format(func.getName(), func.getEntryPoint()))
+    entry_point = func.getEntryPoint()
+    newDict = {"name": func.getName(), "address": entry_point}
+    funcDicts.append(newDict)
+    print("Function: {} @ 0x{}".format(func.getName(), entry_point))
+
+    references = getReferencesTo(entry_point)
+    for xref in references:
+        print(xref)
